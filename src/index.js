4498b4f6632d594c4d4c13cbd3807a89111acd4c
thethtarzin111 <137447082+thethtarzin111@users.noreply.github.com>
2023-07-01 17:02:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 const submitButton = document.getElementById("submit-data");
-
+let y = 0;
 submitButton.addEventListener("click", () => {
   let table = document.getElementById("data-table");
-  let row = table.insertRow(4);
-  let adminY = document.getElementById("input-admin");
-  let column1 = row.insertCell(0);
-  let column2 = row.insertCell(1);
-  let column3 = row.insertCell(2);
-  let column4 = row.insertCell(3);
-  column1.innerHTML = document.getElementById("input-username").value;
-  column2.innerHTML = document.getElementById("input-email").value;
-  column3.innerHTML = document.getElementById("input-address").value;
 
-  if (adminY.checked) {
-    column4.innerHTML = "X";
+  let targetTDs = table.querySelectorAll("tr > td:first-child");
+  let columns = table.querySelectorAll("tr>td");
+  for (let i = 1; i < targetTDs.length + 1; i++) {
+    let td = targetTDs[i];
+    //console.log(td.innerHTML);
+
+    if (td.innerHTML === document.getElementById("input-username").value) {
+      //console.log("The same username");
+      columns[5 + y].innerHTML = document.getElementById("input-email").value;
+      columns[6 + y].innerHTML = document.getElementById("input-address").value;
+      if (document.getElementById("input-admin").checked) {
+        columns[7 + y].innerHTML = "X";
+      } else {
+        columns[7 + y].innerHTML = "-";
+      }
+
+      break;
+    } else {
+      y = y + 4;
+      if (
+        i > targetTDs.length &&
+        td.innerHTML !== document.getElementById("input-username").value
+      ) {
+        let row = table.insertRow(4);
+        let adminY = document.getElementById("input-admin");
+        let column1 = row.insertCell(0);
+        let column2 = row.insertCell(1);
+        let column3 = row.insertCell(2);
+        let column4 = row.insertCell(3);
+        column1.innerHTML = document.getElementById("input-username").value;
+        column2.innerHTML = document.getElementById("input-email").value;
+        column3.innerHTML = document.getElementById("input-address").value;
+
+        if (adminY.checked) {
+          column4.innerHTML = "X";
+        }
+      }
+    }
   }
-  console.log(table.rows.length);
 });
 
 let emptyTableButton = document.getElementById("empty-table");
@@ -24,4 +50,28 @@ emptyTableButton.addEventListener("click", () => {
   for (let i = 0; i <= table.rows.length + 1; i++) {
     table.deleteRow(1);
   }
+});
+
+const firstColData = document.getElementById("print-firstCol");
+firstColData.addEventListener("click", () => {
+  let table = document.getElementById("data-table");
+  let targetTDs = table.querySelectorAll("tr > td:first-child");
+
+  for (let i = 1; i < targetTDs.length; i++) {
+    let td = targetTDs[i];
+    console.log(td.innerHTML);
+  }
+});
+
+const uploadButton = document.getElementById("input-image");
+uploadButton.addEventListener("click", () => {
+  let table = document.getElementById("data-table");
+  let row = table.insertRow(1);
+  let column5 = row.insertCell(0);
+
+  let file = document.getElementsByName("input-image")[0].files[0];
+  let src = URL.createObjectURL(file);
+  let image = document.createElement("img");
+  image.src = src;
+  column5.appendChild(image);
 });
